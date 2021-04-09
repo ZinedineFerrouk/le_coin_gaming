c2b0715e153f3116736490180895ec3b77d30e22
said27 <said.adraou@gmail.com>
2021-04-09 11:30:58 +0200

    gestion des annonces utilisateur

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,6 +20,19 @@ class ProfilController extends AbstractController
     {
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/mesAnnonces", name="profil_annonce", methods={"GET"})
+     */
+    public function show(): Response
+    {
+        $user = $this->getUser();
+        $annonces = $user->getAnnonces();
+        return $this->render('profil/mesAnnonces.html.twig', [
+            "annonces" => $annonces,
         ]);
     }
 }
