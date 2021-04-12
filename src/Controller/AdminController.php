17ed5a8195693140d38de5282b39aba8884a1c9c
said27 <said.adraou@gmail.com>
2021-04-12 16:07:30 +0200

    integration partie ADMIN

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Annonce;
+use App\Entity\Jeu;
+use App\Form\AnnonceType;
+use App\Form\JeuType;
+use App\Repository\AnnonceRepository;
+use App\Repository\JeuRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+        ]);
+    }
+
+
+    /**
+     * @Route("/annonces", name="admin_annonces", methods={"GET"})
+     */
+    public function show_annonce(AnnonceRepository $annonceRepository): Response
+    {
+        $annonces = $annonceRepository->findAllAnnonce();
+        return $this->render('admin/annonces.html.twig', [
+            'annonces' => $annonces,
+        ]);
+    }
+
+    /**
+     * @Route("/jeux", name="admin_jeu", methods={"GET"})
+     */
+    public function show_jeu(JeuRepository $jeuRepository): Response
+    {
+        return $this->render('admin/jeux.html.twig', [
+            'jeus' => $jeuRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new_jeu", name="admin_jeu_new", methods={"GET","POST"})
+     */
+    public function new_jeu(Request $request): Response
+    {
+        $jeu = new Jeu();
+        $form = $this->createForm(JeuType::class, $jeu);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($jeu);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_jeu');
+        }
+
+        return $this->render('admin/jeux_new.html.twig', [
+            'jeu' => $jeu,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/annonces/{id}/edit", name="admin_annonce_edit", methods={"GET","POST"})
+     */
+    public function edit_annonce(Request $request, Annonce $annonce): Response
+    {
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_annonces');
+        }
+
+        return $this->render('admin/annonce_edit.html.twig', [
+            'annonce' => $annonce,
+            'form' => $form->createView(),
+        ]);
+    }
+}
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,16 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Admnistrateur' => 'ROLE_ADMIN',
+                    'Super Admin' => 'SUPER_ADMIN'
+                ],
+                'expanded' => true,
+                'multiple' => true,
+                'label' => 'Rôles' 
+            ])
             ->add('password')
             ->add('nom')
             ->add('prenom')
